d0fe7a342ea29ef1043005d9fa57d763333970a1
trmttm <96355189+trmttm@users.noreply.github.com>
2021-12-27 21:05:06 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='os_identifier',
+    version='1.0.0',
+    packages=['os_identifier'],
+    url='',
+    license='',
+    author='Taro Yamaka',
+    author_email='t.yamaka.1213@gmail.com',
+    description=''
+)
