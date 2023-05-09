5424fc26040f73b75bd9ec28a04070428e9015bb
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-09 15:15:25 +0900

    Add NEWLINE_SYMBOL

--- os_identifier/__init__.py
+++ os_identifier/__init__.py
@@ -5,12 +5,14 @@ WINDOWS = 'Windows'
 MAC = 'Mac'
 
 OS = MAC
+NEWLINE_SYMBOL = '\n'
 if sys.platform == "linux" or sys.platform == "linux2":
     OS = LINUX
 elif sys.platform == "darwin":
     OS = MAC
 elif sys.platform == "win32":
     OS = WINDOWS
+    NEWLINE_SYMBOL = '¥¥'
 
 is_linux = OS == LINUX
 is_windows = OS == WINDOWS
