14ede9c77dc31e2e01b3d43c40cade0f7c80446a
trmttm <96355189+trmttm@users.noreply.github.com>
2023-05-10 09:21:40 +0900

    update os_identifier

--- os_identifier/__init__.py
+++ os_identifier/__init__.py
@@ -5,6 +5,7 @@ WINDOWS = 'Windows'
 MAC = 'Mac'
 
 OS = MAC
+DIRECTORY_SEPARATOR = '/'
 NEW_LINE_SYMBOL = '\n'
 if sys.platform == "linux" or sys.platform == "linux2":
     OS = LINUX
@@ -12,7 +13,8 @@ elif sys.platform == "darwin":
     OS = MAC
 elif sys.platform == "win32":
     OS = WINDOWS
-    NEW_LINE_SYMBOL = '¥¥'
+    DIRECTORY_SEPARATOR = '¥¥'
+    NEW_LINE_SYMBOL = '¥n'
 
 is_linux = OS == LINUX
 is_windows = OS == WINDOWS
